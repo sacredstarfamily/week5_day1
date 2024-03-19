6d28461895d22bcfef2229610086b5dbde670671
Caleb <seeloveinfinite@gmail.com>
2024-03-19 07:32:59 -0700

    initial

--- Script-1.sql
+++ Script-1.sql
@@ -332,3 +332,20 @@ FROM (
 	OFFSET 5
 	LIMIT 5
 ) AS example ORDER BY num_names;
+
+
+-- Start Home work *** 
+-- 1. How many actors are there with the last name ‘Wahlberg’?
+SELECT first_name ,last_name FROM actor WHERE last_name = 'Wahlberg'
+-- there are 2 actors with the last name Wahlberg
+
+--2. How many payments were made between $3.99 and $5.99?
+
+--3. What films have exactly 7 copies? (search in inventory)
+--4. How many customers have the first name ‘Willie’?
+--5. What store employee (get the id) sold the most rentals (use the rental table)?
+--6. How many unique district names are there?
+--7. What film has the most actors in it? (use film_actor table and get film_id)
+--8. From store_id 1, how many customers have a last name ending with ‘es’? (use customer table)
+--9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers with ids between 380 and 430? (use group by and having > 250)
+--10. Within the film table, how many rating categories are there? And what rating has the most movies total?
